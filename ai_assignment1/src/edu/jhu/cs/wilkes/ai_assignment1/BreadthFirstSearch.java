cece0ccd96ec13d13f663ded7c4b417d066fc241
Ian Wilkes <ianm.wilkes@gmail.com>
2015-02-11 00:19:45 -0500

    added basic code to remember visited nodes to bfs

--- ai_assignment1/src/edu/jhu/cs/wilkes/ai_assignment1/BreadthFirstSearch.java
+++ ai_assignment1/src/edu/jhu/cs/wilkes/ai_assignment1/BreadthFirstSearch.java
@@ -1,13 +1,15 @@
 package edu.jhu.cs.wilkes.ai_assignment1;
 
-import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.LinkedList;
 import java.util.Queue;
+import java.util.Set;
 
 public class BreadthFirstSearch implements GridSearchAlgorithm {
 	
 	private Queue<GridSearchPath> pathsToCheck;
 	private GridSearchMap gridMap;
+	private Set<Position> checkedNodes;
 	private int nodesExpanded;
 	/**
 	 * Constructor for the breadth first search. 
@@ -23,14 +25,23 @@ public class BreadthFirstSearch implements GridSearchAlgorithm {
 	public GridSearchPath gridSearch(Position start, Position goal) {
 		GridSearchPath currPath = new GridSearchPath(new GridSearchPath(goal), start);
 		pathsToCheck.add(currPath);
+		checkedNodes = new HashSet<Position>();
+		
 		while(!pathsToCheck.isEmpty()) {
-			this.nodesExpanded++;
 			currPath = pathsToCheck.remove();
+			if (checkedNodes.contains(currPath.getLastNode())) {
+				continue;
+			}
+			checkedNodes.add(currPath.getLastNode());
+			this.nodesExpanded++;
+			
 			if (currPath.getLastNode().isGoal()) {
 				return currPath;
 			} else {
 				for (Position p:  this.gridMap.getAdjacentNodes(currPath.getLastNode())) {
-					pathsToCheck.add(new GridSearchPath(currPath, p));
+					if (!checkedNodes.contains(p)) {
+						pathsToCheck.add(new GridSearchPath(currPath, p));
+					}
 				}
 			}
 		}
