8a3ad4448b8a3b8d743b613b0318f787dee1c1a2
Ian Wilkes <ianm.wilkes@gmail.com>
2015-02-12 22:49:57 -0500

    test all args

--- ai_assignment1/src/edu/jhu/cs/wilkes/ai_assignment1/GridSearchMain.java
+++ ai_assignment1/src/edu/jhu/cs/wilkes/ai_assignment1/GridSearchMain.java
@@ -19,8 +19,8 @@ public class GridSearchMain {
 	 */
 	public static void main(String[] args) {
 		// TODO test args 
-		if (args.length >= 1) {
-			System.out.println(args[0]);
+		for (int i = 0; i < args.length; i++) {
+			System.out.println(args[i]);
 		}
 		Position start;
 		Position goal;
